1c8b65d3375aa7f20a19e43a4a00e7821fb99a09
Andrew Phung <andrewphung@a-tg4wygtfm.carfax.cfx>
2019-07-12 13:52:04 -0400

    config for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const hbs = require('hbs');
 const log = console.log;
 
 const app = express();
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, '../public');
@@ -105,6 +106,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    log('Server started on port 3000.')
+app.listen(port, () => {
+    log(`Server started on port ${port}.`);
 });
